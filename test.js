9bb0a9f96df72f903057ac1c4f6f302d32cf4f1b
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2019-06-28 12:30:17 +0800

    fix: Corrected duplicate 'from' slugs and added a test to check for future duplicate entries

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
 const assert = require('assert').strict;
 const fs = require('fs');
-
 const redirects = JSON.parse(fs.readFileSync('./redirects.json', 'utf8'));
+const fromSlugs = [];
 
 redirects.forEach(obj => {
   const from = obj.from;
   
   assert.notStrictEqual(from, '/');
   assert.notStrictEqual(from, '/ghost');
+  
+  fromSlugs.push(from);
 });
+
+const duplicates = fromSlugs.reduce((acc, curr, i, arr) => {
+  if (arr.indexOf(curr) !== i && acc.indexOf(curr) < 0) {
+    acc.push(curr);
+  }
+
+  return acc;
+}, []);
+
+assert.deepStrictEqual(duplicates.length, 0);
