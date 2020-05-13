c51c20542ad86fb7a280b8f6a699c2b976842c9f
Mrugesh Mohapatra <me@raisedadead.com>
2020-05-13 15:13:16 +0530

    fix: add tests for /news in slug

--- test.js
+++ test.js
@@ -1,31 +1,36 @@
-const assert = require('assert').strict;
-const fs = require('fs');
-const redirects = JSON.parse(fs.readFileSync('./redirects.json', 'utf8'));
+const assert = require("assert").strict;
+const fs = require("fs");
+const redirects = JSON.parse(fs.readFileSync("./redirects.json", "utf8"));
 const fromSlugs = [];
+const newsSubPathRE = /^\/news\//;
 
 redirects.forEach((obj, i) => {
   const from = obj.from;
   const to = obj.to;
-  const mediumRedirect = to.includes('https://medium.com/');
+  const mediumRedirect = to.includes("https://medium.com/");
 
   // Skip the first five redirect objects
   if (i > 4) {
-    
     // 'from' is not '/' or '/ghost'
-    assert.notStrictEqual(from, '/');
-    assert.notStrictEqual(from, '/ghost');
+    assert.notStrictEqual(from, "/");
+    assert.notStrictEqual(from, "/ghost");
 
     if (!mediumRedirect) {
       // First characters are '/'
-      assert.deepStrictEqual(from[0], '/');
-      assert.deepStrictEqual(to[0], '/');
+      assert.deepStrictEqual(from[0], "/");
+      assert.deepStrictEqual(to[0], "/");
     }
 
     // Last characters are not '/'
-    assert.notStrictEqual(from[from.length - 1], '/');
+    assert.notStrictEqual(from[from.length - 1], "/");
     // 'to' is not News front page
     if (to.length > 1) {
-      assert.notStrictEqual(to[to.length - 1], '/');
+      assert.notStrictEqual(to[to.length - 1], "/");
+    }
+
+    // the slugs should not include `/news` in the paths
+    if (newsSubPathRE.test(from) || newsSubPathRE.test(to)) {
+      throw "Found non-complaint slug in from:" + from + " or/and to: " + to;
     }
 
     fromSlugs.push(from);
